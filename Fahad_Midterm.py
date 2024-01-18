12a4782fc9a687564782dfdeac2eb2de10713694
muhamad fahad <85051677+fahad123-arc@users.noreply.github.com>
2024-01-18 21:49:32 +0500

    Add files via upload

---
+++ Fahad_Midterm.py
@@ -0,0 +1,44 @@
+#Muhammad Fahad Afroze
+#Mid term Examination 
+#Q1 - Write a Python program to do arithmetical operations addition and division.
+num1=int(input("Enter the number:", ))
+num2=int(input("Enter the number:", ))
+addition=num1+num2
+print("The sum are :", addition)
+
+# now for division
+divident=int(input("Enter the number:", ))
+divisor=int(input("Enter the number:", ))
+Divide=divident/divisor
+print("The division of ",divident,"&", divisor,"=",Divide)
+
+
+
+#Q2 - Write a Python program to find the area of a triangle
+base_of_triangle_=int(input("Enter the number:", ))
+height_of_trianlge=int(input("Enter the number:", ))
+Area_of_triangle=1/2*base_of_triangle_*height_of_trianlge
+print("The area of triangle is :",Area_of_triangle)
+
+#Q3 - Write a Python program to convert Celsius to Fahrenheit.
+
+cel=int(input("Enter the number:", ))
+Fahrenheit=cel*98.6/37
+print("Celsius to Fahrenheit is :" , Fahrenheit)
+
+#Q4 - Write a Python program that return all datatypes that we discussed in the class.
+
+number1=int(input("Enter the number:", ))
+print("The Data type of", number1, "is", type(number1))
+#answer will be integer
+number2=float(input("Enter the number:", ))
+print("The Data type of", number2, "is", type(number2))
+#answer will be in float
+number3=str(input("Enter the number:", ))
+print("The Data type of", number3, "is", type(number3))
+#answer will be str 
+x=True
+print("the data type of", x, "is", type(x) )
+#answer will be boolian
+
+
